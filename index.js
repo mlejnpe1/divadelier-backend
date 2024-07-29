aa3a6b50a2637fdd4d380ba8e3e0e15f5c030e45
Petr Mlejnek <mlejnpe1@uhk.cz>
2024-07-29 02:25:35 +0200

    working backend

--- index.js
+++ index.js
@@ -1,65 +1,14 @@
-/*const connect = require ("./connect")
-const express = require ("express")
-const bodyParser = require('body-parser');
-const cors = require ("cors")
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const events = require("./Routes/eventsRoutes")
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const app = express()
-const PORT = process.env.PORT || 3001
-
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json());
-app.use(events)
-
-const users = [
-    {
-      username: 'adela',
-      password: bcrypt.hashSync('password123', 10),
-    },
-  ];
-
-  const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (token == null) return res.sendStatus(401);
-  
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403);
-      req.user = user;
-      next();
-    });
-  };
-
-  app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-  
-    const user = users.find(u => u.username === username);
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const accessToken = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET);
-      res.json({ success: true, token: accessToken });
-    } else {
-      res.json({ success: false });
-    }
-  });
-
-app.listen(PORT, ()=>{
-    connect.connectToServer()
-    console.log(`Server is running on port: ${PORT}`);
-})*/
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const eventsRoutes = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { ObjectId } = require('mongodb');
 
 const mongoURL = "mongodb+srv://mlejnpe1:PWx09xIuu3qd2JAH@cluster0.tvfgjoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 const PORT = 3001;
+
 
 const app = express();
 app.use(express.json());
@@ -84,16 +33,79 @@ try {
 
 const database = client.db("divadelier-deploy");
 
+//Login apis
+
+const users = [
+  {
+    username: 'adela',
+    password: bcrypt.hashSync('password123', 10),
+  },
+];
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, "muj_tajny_token", (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(u => u.username === username);
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const accessToken = jwt.sign({ username: user.username }, "muj_tajny_token");
+    res.json({ success: true, token: accessToken });
+  } else {
+    res.json({ success: false });
+  }
+});
+
 //Events apis
+
+//Get all events
 app.get("/events", async (req, res)=>{
   let data = await database.collection("Events").find({}).toArray();
 
   if (data.length > 0) {
+    data.sort((a, b) => new Date(b.date) - new Date(a.date))
     res.json(data);
   } else {
     throw new Error("Data was not found.");
   }
 })
+
+//add new event
+app.post("/events", async (req, res) => {
+  let mongoObject = {
+    date: req.body.date,
+    location: req.body.location,
+    info: req.body.info,
+  };
+  let data = await database.collection("Events").insertOne(mongoObject);
+
+  res.json(data);
+});
+
+//delete event
+app.delete("/events/:id", async (req, res) => {
+  const eventId = new ObjectId(req.params.id);
+  try {
+    if(database.collection("Events").findOne({_id: eventId})){
+      await database.collection("Events").deleteOne({_id: eventId});
+      res.json({ message: 'Event deleted' });
+    }else{
+      console.log("Event not found");
+    }
+  } catch (err) {
+    console.error('Error deleting event:', err);
+    res.status(500).json({ message: 'Internal Server Error', error: err.message });
+  }
+});
 
 app.listen(PORT, ()=>{
   console.log("Server is running and listening at port: " + PORT);
